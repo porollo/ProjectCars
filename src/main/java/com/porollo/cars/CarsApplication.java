1648c02b24db78703293cc7a5d473701a7d433fa
Dmitriy S. Porollo <d.porollo@gmail.com>
2019-07-05 20:06:56 +0300

    Some minor angular changes

--- src/main/java/com/porollo/cars/CarsApplication.java
+++ src/main/java/com/porollo/cars/CarsApplication.java
@@ -1,34 +1,13 @@
 package com.porollo.cars;
 
-import com.porollo.cars.model.User;
-import com.porollo.cars.model.UserDetails;
-import com.porollo.cars.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CarsApplication {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(CarsApplication.class, args);
     }
-
-    @Bean
-    CommandLineRunner init(UserRepository userRepository) {
-        return args -> {
-            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
-                User user = new User(name, name.toLowerCase() + "@domain.com");
-                userRepository.save(user);
-            });
-            userRepository.findAll().forEach(System.out::println);
-        };
-    }
-
-
+    
 }
