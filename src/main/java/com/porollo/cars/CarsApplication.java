45fb94c21f18600d96b22ce320559134d735d570
Dmitriy S. Porollo <d.porollo@gmail.com>
2019-07-05 18:14:39 +0300

    Added angular client

--- src/main/java/com/porollo/cars/CarsApplication.java
+++ src/main/java/com/porollo/cars/CarsApplication.java
@@ -6,6 +6,9 @@ import com.porollo.cars.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CarsApplication {
@@ -14,6 +17,17 @@ public class CarsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarsApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
     }
 
 
