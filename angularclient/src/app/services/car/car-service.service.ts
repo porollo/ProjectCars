1e8679c802128f79efb7fae3cb3344a2e6cdbbd3
Dmitriy S. Porollo <d.porollo@gmail.com>
2019-07-05 21:29:00 +0300

    Some minor angular changes

--- angularclient/src/app/services/car/car-service.service.ts
+++ angularclient/src/app/services/car/car-service.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from "rxjs";
+import {Car} from "../../models/car/car";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class CarServiceService {
+@Injectable()
+export class CarService {
 
-  constructor() { }
+  private readonly usersUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.usersUrl = 'http://localhost:8080/cars';
+  }
+
+  public findAll(): Observable<Car[]> {
+    return this.http.get<Car[]>(this.usersUrl);
+  }
+
+  public save(car: Car) {
+    return this.http.post<Car>(this.usersUrl, car);
+  }
 }
--- angularclient/src/app/services/user/user-service.service.ts
+++ angularclient/src/app/services/user/user-service.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {User} from "../../models/user/user";
+import {Observable} from "rxjs";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserServiceService {
+@Injectable()
+export class UserService {
 
-  constructor() { }
+  private readonly usersUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.usersUrl = 'http://localhost:8080/users';
+  }
+
+  public findAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl);
+  }
+
+  public save(user: User) {
+    return this.http.post<User>(this.usersUrl, user);
+  }
 }
